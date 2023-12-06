280c16ff6a837b55b8ffbc9b76cb7f6a59eba348
joao.leite <joao.leiteu@somagrupo.com.br>
2023-12-06 18:21:04 -0300

    feat: first commit

---
+++ src/pages/shared/Group/MapList.tsx
@@ -0,0 +1,26 @@
+import MapItem from "../../../common/components/CardItem";
+import { LoadingComponent } from "../../../common/styled/LoadingComponent";
+import { MapListContainer } from "./style";
+
+export default function MapList({ maps = [] }: any) {
+  return (
+    <>
+      {maps.length > 0 ? (
+        <MapListContainer>
+          {maps.map((map: any) => {
+            return (
+              <MapItem
+                key={map.id_map}
+                id={map.id}
+                title={map.name}
+                imgBg={map?.thumb_url}
+              />
+            );
+          })}
+        </MapListContainer>
+      ) : (
+        <LoadingComponent size={30} />
+      )}
+    </>
+  );
+}
--- src/pages/shared/Group/index.tsx
+++ src/pages/shared/Group/index.tsx
@@ -1,2 +1,91 @@
-const Group = () => <div>Grupo</div>
+import Header from "../../../common/components/Header/Header";
+// import {
+//   ContainerDiv,
+//   H1Title,
+//   H2Title,
+//   PageContent,
+// } from "../../../common/styled/main.styled";
+import { GroupCreatedBy, GroupHeader } from "./style";
+import MapList from "./MapList";
+
+const Group = () => {
+  const group = {
+    name: "Exploração do 1 Andar",
+    owner: {
+      name: "Maxwell Ebert",
+    },
+    created_at: "2023-10-15T10:20:00.000Z",
+  };
+
+  const members = [
+    "Annie Price",
+    "Ted Kovacek",
+    "Brandon Jakubowski",
+    "Annie Price",
+    "Ted Kovacek",
+    "Brandon Jakubowski",
+    "Annie Price",
+    "Ted Kovacek",
+    "Brandon Jakubowski",
+    "Annie Price",
+    "Ted Kovacek",
+    "Brandon Jakubowski",
+  ];
+
+  const maps = [
+    {
+      id: 1,
+      id_map: 111,
+      name: "Pavilhão 1",
+      thumb_url: "",
+    },
+    {
+      id: 2,
+      id_map: 222,
+      name: "Pavilhão 2",
+      thumb_url: "",
+    },
+    {
+      id: 3,
+      id_map: 333,
+      name: "Bloco H",
+      thumb_url: "",
+    },
+    {
+      id: 4,
+      id_map: 444,
+      name: "Bloco E",
+      thumb_url: "",
+    },
+  ];
+
+  return (
+    <>
+      <Header title="Grupo" />
+      <main>
+        {/* <ContainerDiv>
+          <GroupHeader>
+            <H1Title>Grupo: {group.name}</H1Title>
+            <GroupCreatedBy>
+              <strong>Criado em:</strong>{" "}
+              {new Date(group.created_at).toLocaleDateString()}
+            </GroupCreatedBy>
+            <GroupCreatedBy>
+              <strong>Líder:</strong> {group.owner.name}
+            </GroupCreatedBy>
+          </GroupHeader>
+
+          <PageContent>
+            <H2Title>Membros</H2Title>
+            <p>{members.toString()}</p>
+
+            <H2Title>Mapas</H2Title>
+            <MapList maps={maps} />
+          </PageContent>
+        </ContainerDiv> */}
+      </main>
+    </>
+  );
+};
+
 export default Group;
